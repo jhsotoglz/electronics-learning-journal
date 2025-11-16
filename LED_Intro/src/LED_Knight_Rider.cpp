dcb0a33ba89042c10421c6499e779c971fe66d0a
Jesus Soto Gonzalez <jhsotoglz@gmail.com>
2025-11-15 22:09:03 -0600

    Knight Rider Sequence file added

---
+++ LED_Intro/src/LED_Knight_Rider.cpp
@@ -0,0 +1,30 @@
+# include <Arduino.h>
+
+const int LEDS[] = {13, 4, 2}; // Red, Green, Yellow
+const int NUM_LEDS = 3;
+
+void blinkled (int pin, int ms);
+
+void setup () {
+    for (int i = 0; i < NUM_LEDS; i++){
+        pinMode(LEDS[i], OUTPUT);
+    }
+}
+
+void loop () {
+    // Forward sequence
+    for (int i = 0; i < NUM_LEDS; i++){
+        blinkled(LEDS[i], 100);
+    }
+
+    for (int i = NUM_LEDS - 2; i > 0; i--){
+        blinkled(LEDS[i], 100);
+    }
+}
+
+void blinkled (int pin, int ms) {
+    digitalWrite(pin, HIGH);
+    delay(ms);
+    digitalWrite(pin, LOW);
+    delay(ms);
+}
