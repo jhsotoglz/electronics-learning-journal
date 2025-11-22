df1b2390f430b21d072cf73e3e124c6c15bb08ba
Jesus Soto Gonzalez <jhsotoglz@gmail.com>
2025-11-21 19:18:37 -0600

    F1 start simulation added

--- LED_Intro/src/LED_TrafficLight.cpp
+++ LED_Intro/src/LED_TrafficLight.cpp
@@ -1,42 +1,42 @@
-# include <Arduino.h>
+// # include <Arduino.h>
 
-const int RED_LED_PIN = 13;
-const int GREEN_LED_PIN = 4;
-const int YELLOW_LED_PIN = 2;
+// const int RED_LED_PIN = 13;
+// const int GREEN_LED_PIN = 4;
+// const int YELLOW_LED_PIN = 2;
 
-const int RED_TIME = 10000;
-const int GREEN_TIME = 10000;
-const int YELLOW_TIME = 3000;
+// const int RED_TIME = 10000;
+// const int GREEN_TIME = 10000;
+// const int YELLOW_TIME = 3000;
 
-void stoplight_sequence ();
+// void stoplight_sequence ();
 
-void setup () {
-    pinMode(RED_LED_PIN, OUTPUT);
-    pinMode(GREEN_LED_PIN, OUTPUT);
-    pinMode(YELLOW_LED_PIN, OUTPUT);
-}
+// void setup () {
+//     pinMode(RED_LED_PIN, OUTPUT);
+//     pinMode(GREEN_LED_PIN, OUTPUT);
+//     pinMode(YELLOW_LED_PIN, OUTPUT);
+// }
 
-void loop () {
-    stoplight_sequence();
-}
+// void loop () {
+//     stoplight_sequence();
+// }
 
-void stoplight_sequence () {
+// void stoplight_sequence () {
     
-    digitalWrite(GREEN_LED_PIN, HIGH);
+//     digitalWrite(GREEN_LED_PIN, HIGH);
 
-    delay(GREEN_TIME);
+//     delay(GREEN_TIME);
 
-    digitalWrite(GREEN_LED_PIN, LOW);
+//     digitalWrite(GREEN_LED_PIN, LOW);
 
-    digitalWrite(YELLOW_LED_PIN, HIGH);
+//     digitalWrite(YELLOW_LED_PIN, HIGH);
 
-    delay(YELLOW_TIME);
+//     delay(YELLOW_TIME);
 
-    digitalWrite(YELLOW_LED_PIN, LOW);
+//     digitalWrite(YELLOW_LED_PIN, LOW);
 
-    digitalWrite(RED_LED_PIN, HIGH);
+//     digitalWrite(RED_LED_PIN, HIGH);
 
-    delay(RED_TIME);
+//     delay(RED_TIME);
 
-    digitalWrite(RED_LED_PIN, LOW);
-}
+//     digitalWrite(RED_LED_PIN, LOW);
+// }
